33832e59e7adb2790c4c1cf897fb4b98391a1572
shayle664 <160335264+shayle664@users.noreply.github.com>
2025-05-30 10:25:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
-=======
-# pylint: skip-file
-
->>>>>>> 95d73b0 (add files)
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
-=======
-
->>>>>>> 95d73b0 (add files)
-    return jsonify(success=True,message='Hello World')
+    return jsonify(success=True, message='Hello World')
 
 @app.route('/<name>')
 def hello(name):
-<<<<<<< HEAD
-=======
-
->>>>>>> 95d73b0 (add files)
-    return jsonify(success=True,message=f'Hello {name}')
-
-
+    return jsonify(success=True, message=f'Hello {name}')
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8000)
